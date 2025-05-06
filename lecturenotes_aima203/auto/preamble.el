f5236f7f1a287bfbb345c379c2a15e1da0b6863a
Sandesh Thakuri <sandeshthakuri111@gmail.com>
2025-05-06 14:41:18 +0545

    python code bisection

--- lecturenotes_aima203/auto/preamble.el
+++ lecturenotes_aima203/auto/preamble.el
@@ -1,17 +1,19 @@
+;; -*- lexical-binding: t; -*-
+
 (TeX-add-style-hook
  "preamble"
  (lambda ()
    (setq TeX-command-extra-options
          "-shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("tcolorbox" "most") ("fncychap" "Bjornstrup")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("geometry" "") ("subcaption" "") ("graphicx" "") ("wrapfig" "") ("multicol" "") ("mdframed" "") ("tikz" "") ("pgfplots" "") ("amsmath" "") ("amssymb" "") ("amsthm" "") ("mathtools" "") ("amsfonts" "") ("titling" "") ("tcolorbox" "most") ("listings" "") ("xcolor" "") ("hyperref" "") ("fncychap" "Bjornstrup") ("fancyhdr" "") ("subfiles" "")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "geometry"
@@ -30,9 +32,11 @@
     "amsfonts"
     "titling"
     "tcolorbox"
+    "listings"
     "hyperref"
     "fncychap"
-    "fancyhdr")
+    "fancyhdr"
+    "subfiles")
    (LaTeX-add-mdframed-mdfdefinestyles
     "myframe")
    (LaTeX-add-amsthm-newtheorems
