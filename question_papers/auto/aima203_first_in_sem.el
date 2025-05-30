f452eacdc73f14d2a72bd256aca5883d1e04cf06
Sandesh <sandeshthakuri111@gmail.com>
2025-05-30 08:13:40 +0545

    question paper

---
+++ question_papers/auto/aima203_first_in_sem.el
@@ -0,0 +1,27 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "aima203_first_in_sem"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("exam" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("preamble_question" "") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "exam"
+    "exam12"
+    "preamble_question"
+    "ulem"))
+ :latex)
+
