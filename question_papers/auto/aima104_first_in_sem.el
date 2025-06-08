2e095bd8e3a2510da0e32ecd961ee9ab0edd4014
Sandesh Thakuri <sandeshthakuri111@gmail.com>
2025-06-08 14:54:34 +0545

    question aima104

---
+++ question_papers/auto/aima104_first_in_sem.el
@@ -0,0 +1,17 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "aima104_first_in_sem"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("exam" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("preamble_question" "") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "exam"
+    "exam12"
+    "preamble_question"
+    "ulem"))
+ :latex)
+
