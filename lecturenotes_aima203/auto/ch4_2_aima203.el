ec916b4a289a6fe275008c5d9ddd788495914a51
Sandesh Thakuri <sandeshthakuri111@gmail.com>
2025-06-11 14:29:01 +0545

    eigen-values

---
+++ lecturenotes_aima203/auto/ch4_2_aima203.el
@@ -0,0 +1,15 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "ch4_2_aima203"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("subfiles" "aima203_lecturenotes_ku.tex")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "subfiles"
+    "subfiles10")
+   (LaTeX-add-labels
+    "cubic"))
+ :latex)
+
