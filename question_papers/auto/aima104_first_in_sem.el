b26544a0116a17643bedc173d3e0c4102fbc5a03
Sandesh Thakuri <sandeshthakuri111@gmail.com>
2025-06-16 14:32:48 +0545

    question_aima104

--- question_papers/auto/aima104_first_in_sem.el
+++ question_papers/auto/aima104_first_in_sem.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("exam" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("preamble_question" "") ("ulem" "normalem")))
+                     '(("preamble_question_small" "") ("ulem" "normalem")))
    (TeX-run-style-hooks
     "latex2e"
     "exam"
     "exam12"
-    "preamble_question"
+    "preamble_question_small"
     "ulem"))
  :latex)
 
